c899818f36461693e6d5e8a12566eb78012d0247
nhadziosma1 <nhadziosma1@etf.unsa.ba>
2018-12-19 18:14:15 +0100

    nista novo

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Gradovi su:\n" + ispisiGradove());
         glavniGrad();
+        aaaa
     }
 }
